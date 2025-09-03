a3e3e86347a6eaba71676281d510f270232d2d66
Dnish123 <danishrazauddinansari@gmail.com>
2025-09-03 21:13:33 +0530

    Fix the group notification bug

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ if uploaded_file is not None:
     #fetch unique users
 
     user_list = df['user'].unique().tolist()
-    user_list.remove('group_notification')
+
+    if "group_notification" in user_list:  # safe check
+        user_list.remove("group_notification")
+
     user_list.sort()
-    user_list.insert(0,"Overall")
+    user_list.insert(0, "Overall")
+
     selected_user = st.sidebar.selectbox("Show Analysis wrt",user_list)
     if st.sidebar.button("Show Analysis"):
         # stats area
