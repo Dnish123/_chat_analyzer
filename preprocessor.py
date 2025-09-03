11658e474f60ab60309863cab337f0bc506f0aa5
Dnish123 <danishrazauddinansari@gmail.com>
2025-09-03 20:30:51 +0530

    Fix: robust regex for WhatsApp chat parsing

--- preprocessor.py
+++ preprocessor.py
@@ -1,7 +1,8 @@
 import re
 import pandas as pd
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}(?:\s?[apAP][mM])?\s-\s'
+
 
     messages = re.split(pattern, data)[1:]
 
